53e4eca2388dd56e8c9065134df4ff779490e747
XxXRuben <rdazas@unal.edu.co>
2024-11-26 11:33:04 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,32 +1,34 @@
+import re
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = None
+    grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def _init_(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
-        self._asignaturas = asignaturas
-        self.listadoAlumnos = estudiantes
+        self._asignaturas = asignaturas if asignaturas else []
+        self.listadoAlumnos = estudiantes if estudiantes else []
 
-    def listadoAsignaturas(self, kwargs):
-        for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x))
+    def listadoAsignaturas(self, **kwargs):
+        for nombre_asignatura in kwargs.values():
+            self._asignaturas.append(Asignatura(nombre_asignatura, None))
 
-    def agregarAlumno(self, alumno, lista=[]):
-        lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos + lista
+    def agregarAlumno(self, alumno, otrosAlumnos=None):
+        if isinstance(alumno, str):
+            self.listadoAlumnos.append(alumno)
+        
+        if otrosAlumnos and isinstance(otrosAlumnos, list):
+            self.listadoAlumnos.extend(otrosAlumnos)
 
-    # def __str__(self):
-    #     pass
+        self.listadoAlumnos.sort(key=self._extract_number)
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
+    def _extract_number(self, alumno):
+        match = re.search(r'(\d+)$', alumno)
+        return int(match.group(1)) if match else 0
 
-    @ classmethod
+    @classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+    def _str_(self):
+        return f"Grupo de estudiantes: {self._grupo}"
